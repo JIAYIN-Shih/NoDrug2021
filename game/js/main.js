d1b78ec35ced61021025ef6efd7eea015921ec71
jiayin <72562168+JIAYIN-Shih@users.noreply.github.com>
2021-11-09 01:39:22 +0800

    更新載入畫面+遊戲

--- game/js/main.js
+++ game/js/main.js
@@ -4,10 +4,7 @@ var config = {
     height: 1080,
     physics: {
         default: 'arcade',
-        arcade: {
-            // gravity: { y: 200 } //重量
-            debug: false
-        }
+        arcade: { debug: false }
     },
     scene: {
         preload: preload,
@@ -29,41 +26,167 @@ var gameOver = false;
 var _this;
 var groundmusic;
 
+var loadtime = 0
+
+var bg//初始化背景
+var loadimg
+
 function preload() {
-    // ===========================載入素材===========================
-    this.load.image('sky', 'img/sky.jpg');
-    this.load.image('ground', 'img/ground.png');
-    this.load.image('house1', 'img/house1.png');
-    this.load.image('house2', 'img/house2.png');
-    this.load.image('clock', 'img/clock.png');
-    this.load.image('healthyHeart', 'img/Heart_1.png');
-    this.load.image('hurtHeart', 'img/Heart_2.png');
+    loadtime = 0;
+    // ===========================顯示設定===========================
+    window.addEventListener('resize', resize);
+    resize();
+    //內建背景
+    bg = this.add.graphics(0, 0);
+    bg.fillStyle(0x0000FF, 1);
+    bg.fillRect(0, 0, 1920, 1080);
 
-    this.load.spritesheet('runningMan',
-        'img/run/run_sprites.png', {
-            frameWidth: 534,
-            frameHeight: 960
+    //LOAD
+    this.load.image('load', 'img/load.gif');
+    this.load.spritesheet('loads', 'img/loads.png', { frameWidth: 1186, frameHeight: 588 });
+
+    this.load.on('progress', function (value) {
+        console.log(value);
+    });
+
+
+    this.load.on('complete', function () {
+        if (loadtime == 1) {
+            start();
         }
-    );
+        if (loadimg == 2) {
+            bg.destroy();
+            loadimg.destroy();
+        }
+        loadtime++
+        console.log('complete');
+    });
 
-    this.load.image('goodfood1', 'img/food/good/sweety1.png');
-    this.load.image('goodfood2', 'img/food/good/sweety2.png');
-    this.load.image('goodfood3', 'img/food/good/sweety3.png');
-    this.load.image('goodfood4', 'img/food/good/sweety4.png');
-    this.load.image('goodfood5', 'img/food/good/sweety5.png');
-    this.load.image('badfood1', 'img/food/bad/poison1.png');
-    this.load.image('badfood2', 'img/food/bad/poison2.png');
-    this.load.image('badfood3', 'img/food/bad/poison3.png');
-    this.load.image('badfood4', 'img/food/bad/poison4.png');
-    this.load.image('badfood5', 'img/food/bad/poison5.png');
-
-    // ===========================載入音樂===========================
-    this.load.audio("groundmusic", 'audio/groundmusic.mp3');
-    this.load.audio("eatbad", 'audio/eatbad.mp3');
-    this.load.audio("eatgood", 'audio/eatgood.mp3');
-    this.load.audio("fail", 'audio/fail.mp3');
-    this.load.audio("pass", 'audio/pass.mp3');
 }
+
+function start() {
+    // 背景音樂
+    groundmusic = _this.sound.add('groundmusic', {
+        volume: 0.2
+    });
+    groundmusic.play();
+
+    //背景        
+    sky1 = _this.physics.add.sprite(0, 0, 'sky').setOrigin(0, 0).refreshBody();
+    sky2 = _this.physics.add.sprite(3840, 0, 'sky').setOrigin(0, 0).refreshBody();
+
+    // 房子
+    house1 = _this.physics.add.sprite(0, 0, 'house1').setOrigin(0, 0);
+    house2 = _this.physics.add.sprite(3500, 0, 'house2').setOrigin(0, 0);
+
+    // 時鐘
+    clock = _this.add.image(1740, 26, 'clock').setOrigin(0, 0).setScale(0.7);
+
+    //愛心
+    hurtHeart1 = _this.add.image(30, 30, 'hurtHeart').setOrigin(0, 0).setScale(0.8);
+    hurtHeart2 = _this.add.image(150, 30, 'hurtHeart').setOrigin(0, 0).setScale(0.8);
+    hurtHeart3 = _this.add.image(270, 30, 'hurtHeart').setOrigin(0, 0).setScale(0.8);
+    healthyHeart1 = _this.add.image(30, 30, 'healthyHeart').setOrigin(0, 0).setScale(0.8);
+    healthyHeart2 = _this.add.image(150, 30, 'healthyHeart').setOrigin(0, 0).setScale(0.8);
+    healthyHeart3 = _this.add.image(270, 30, 'healthyHeart').setOrigin(0, 0).setScale(0.8);
+
+    // 計時器
+    _this.initialTime = 60;
+    text = _this.add.text(1782, 96, _this.initialTime.toString().padStart(2, '0'), {
+        color: '#000',
+        fontSize: '50px',
+        fontFamily: '微軟正黑體'
+    });
+    // 想要顯示幾分幾秒可以用下面這個
+    // text = this.add.text(1600, 50, formatTime(this.initialTime), { color: '#000' ,fontSize: '40px'});
+    _this.countdownTime = 3;
+    countdown = _this.add.text(650, 250, _this.countdownTime.toString().padStart(2, '0'), {
+        color: '#fff',
+        fontSize: '500px',
+        fontFamily: '微軟正黑體'
+    });
+    // 想要顯示幾分幾秒可以用下面這個
+    // countdown = this.add.text(1600, 50, formatTime(this.countdownTime), { color: '#fff' ,fontSize: '40px'});
+
+    // 每秒呼叫一次
+    timedEvent = _this.time.addEvent({
+        delay: 1000,
+        callback: onEvent,
+        callbackScope: _this,
+        loop: true
+    });
+    CountTimedEvent = _this.time.addEvent({
+        delay: 1000,
+        callback: CountdownEvent,
+        callbackScope: _this,
+        loop: true
+    });
+
+    // 創造平台
+    platforms = _this.physics.add.staticGroup();
+    platforms.create(0, 900, 'ground').setOrigin(0, 0).refreshBody();
+
+    //人物
+    player = _this.physics.add.sprite(300, 470, 'runningMan').setOrigin(0, 0).setScale(0.45).refreshBody();
+    player.setGravityY(3500);
+    player.setCollideWorldBounds(true);
+
+    //壞食物
+    badfoods = _this.physics.add.group();
+    //好食物
+    goodfoods = _this.physics.add.group();
+    // 調整食物生成間隔
+    loopfood = _this.time.addEvent({
+        delay: 1500,
+        callback: createfood,
+        callbackScope: _this,
+        loop: true
+    });
+
+    //===========================動畫===========================
+    // 人物動畫
+    _this.anims.create({
+        key: 'run',
+        frames: _this.anims.generateFrameNumbers('runningMan', {
+            start: 0,
+            end: 8
+        }),
+        frameRate: 15,
+        //-1代表會重複loop
+        repeat: -1
+    });
+    _this.anims.create({
+        key: 'stop',
+        frames: [{
+            key: 'runningMan',
+            frame: 0
+        }],
+        frameRate: 1
+    });
+
+    //===========================動畫啟動===========================
+    player.anims.play('run', true);
+
+    //===========================碰撞事件===========================
+    // 人跟地板
+    _this.physics.add.collider(player, platforms);
+
+    // 人跟食物
+    _this.physics.add.overlap(player, badfoods, collectbadfood, null, _this);
+    _this.physics.add.overlap(player, goodfoods, collectgoodfood, null, _this);
+
+    //===========================鍵盤設定===========================
+    cursors = _this.input.keyboard.createCursorKeys();
+
+
+    _this.physics.pause();
+    timedEvent.paused = true;
+    loopfood.paused = true;
+    player.anims.play('stop');
+}
+
+
+
 
 // ===========================自訂 Function Start===========================    
 function resize() {
@@ -87,6 +210,7 @@ function createOnefood(foodgroup, foodlist) {
     getfoodkey = Math.floor(Math.random() * foodlist.length);
     heights = [75, 100];
     food = foodgroup.create(2100, 700 + heights[Math.floor(Math.random() * heights.length)], foodlist[getfoodkey]).setScale(0.3);
+    food.body.setSize(200, 200, true);
     food.setVelocityX(speed);
 }
 
@@ -232,162 +356,115 @@ function renew() {
 function create() {
     _this = this;
 
-    // ===========================顯示設定===========================
-    window.addEventListener('resize', resize);
-    resize();
+    loadimg = this.physics.add.sprite(300, 180, 'load').setOrigin(0, 0);
 
-    // 背景音樂
-    groundmusic = this.sound.add('groundmusic', {
-        volume: 0.2
-    });
-    groundmusic.play();
-
-    //背景        
-    sky1 = this.physics.add.sprite(0, 0, 'sky').setOrigin(0, 0).refreshBody();
-    sky2 = this.physics.add.sprite(3840, 0, 'sky').setOrigin(0, 0).refreshBody();
-
-    // 房子
-    house1 = this.physics.add.sprite(0, 0, 'house1').setOrigin(0, 0);
-    house2 = this.physics.add.sprite(3500, 0, 'house2').setOrigin(0, 0);
-
-    // 時鐘
-    clock = this.add.image(1740, 26, 'clock').setOrigin(0, 0).setScale(0.7);
-
-    //愛心
-    hurtHeart1 = this.add.image(30, 30, 'hurtHeart').setOrigin(0, 0).setScale(0.8);
-    hurtHeart2 = this.add.image(150, 30, 'hurtHeart').setOrigin(0, 0).setScale(0.8);
-    hurtHeart3 = this.add.image(270, 30, 'hurtHeart').setOrigin(0, 0).setScale(0.8);
-    healthyHeart1 = this.add.image(30, 30, 'healthyHeart').setOrigin(0, 0).setScale(0.8);
-    healthyHeart2 = this.add.image(150, 30, 'healthyHeart').setOrigin(0, 0).setScale(0.8);
-    healthyHeart3 = this.add.image(270, 30, 'healthyHeart').setOrigin(0, 0).setScale(0.8);
-
-    // 計時器
-    this.initialTime = 60;
-    text = this.add.text(1782, 96, this.initialTime.toString().padStart(2, '0'), {
-        color: '#000',
-        fontSize: '50px',
-        fontFamily: '微軟正黑體'
-    });
-    // 想要顯示幾分幾秒可以用下面這個
-    // text = this.add.text(1600, 50, formatTime(this.initialTime), { color: '#000' ,fontSize: '40px'});
-    this.countdownTime = 3;
-    countdown = this.add.text(650, 250, this.countdownTime.toString().padStart(2, '0'), {
-        color: '#fff',
-        fontSize: '500px',
-        fontFamily: '微軟正黑體'
-    });
-    // 想要顯示幾分幾秒可以用下面這個
-    // countdown = this.add.text(1600, 50, formatTime(this.countdownTime), { color: '#fff' ,fontSize: '40px'});
-
-    // 每秒呼叫一次
-    timedEvent = this.time.addEvent({
-        delay: 1000,
-        callback: onEvent,
-        callbackScope: this,
-        loop: true
-    });
-    CountTimedEvent = this.time.addEvent({
-        delay: 1000,
-        callback: CountdownEvent,
-        callbackScope: this,
-        loop: true
-    });
-
-    // 創造平台
-    platforms = this.physics.add.staticGroup();
-    platforms.create(0, 900, 'ground').setOrigin(0, 0).refreshBody();
-
-    //人物
-    player = this.physics.add.sprite(300, 470, 'runningMan').setOrigin(0, 0).setScale(0.45).refreshBody();
-    player.setGravityY(3500);
-    player.setCollideWorldBounds(true);
-
-    //壞食物
-    badfoods = this.physics.add.group();
-    //好食物
-    goodfoods = this.physics.add.group();
-    // 調整食物生成間隔
-    loopfood = this.time.addEvent({
-        delay: 1500,
-        callback: createfood,
-        callbackScope: this,
-        loop: true
-    });
-
-    //===========================動畫===========================
-    // 人物動畫
     this.anims.create({
-        key: 'run',
-        frames: this.anims.generateFrameNumbers('runningMan', {
-            start: 0,
-            end: 8
-        }),
-        frameRate: 15,
-        //-1代表會重複loop
+        key: 'startload',
+        frames: this.anims.generateFrameNumbers('loads', { start: 0, end: 47 }),
+        frameRate: 10,
         repeat: -1
     });
-    this.anims.create({
-        key: 'stop',
-        frames: [{
-            key: 'runningMan',
-            frame: 0
-        }],
-        frameRate: 1
-    });
-
-    //===========================動畫啟動===========================
-    player.anims.play('run', true);
-    
-    //===========================碰撞事件===========================
-    // 人跟地板
-    this.physics.add.collider(player, platforms);
-
-    // 人跟食物
-    this.physics.add.overlap(player, badfoods, collectbadfood, null, this);
-    this.physics.add.overlap(player, goodfoods, collectgoodfood, null, this);
-
-    //===========================鍵盤設定===========================
-    cursors = this.input.keyboard.createCursorKeys();
+    loadimg.anims.play('startload', true);
 
     // 起始暫停
-    this.physics.pause();
-    timedEvent.paused = true;
-    loopfood.paused = true;
-    player.anims.play('stop');
+
+    // ===========================載入素材===========================
+    this.load.image('sky', 'img/sky.jpg');
+    this.load.image('ground', 'img/ground.png');
+    this.load.image('house1', 'img/house1.png');
+    this.load.image('house2', 'img/house2.png');
+    this.load.image('clock', 'img/clock.png');
+    this.load.image('healthyHeart', 'img/Heart_1.png');
+    this.load.image('hurtHeart', 'img/Heart_2.png');
+
+    this.load.spritesheet('runningMan',
+        'img/run/run_sprites.png', {
+        frameWidth: 534,
+        frameHeight: 960
+    }
+    );
+
+    this.load.image('goodfood1', 'img/food/good/sweety1.png');
+    this.load.image('goodfood2', 'img/food/good/sweety2.png');
+    this.load.image('goodfood3', 'img/food/good/sweety3.png');
+    this.load.image('goodfood4', 'img/food/good/sweety4.png');
+    this.load.image('goodfood5', 'img/food/good/sweety5.png');
+    this.load.image('badfood1', 'img/food/bad/poison1.png');
+    this.load.image('badfood2', 'img/food/bad/poison2.png');
+    this.load.image('badfood3', 'img/food/bad/poison3.png');
+    this.load.image('badfood4', 'img/food/bad/poison4.png');
+    this.load.image('badfood5', 'img/food/bad/poison5.png');
+
+    // ===========================載入音樂===========================
+    this.load.audio("groundmusic", 'audio/groundmusic.mp3');
+    this.load.audio("eatbad", 'audio/eatbad.mp3');
+    this.load.audio("eatgood", 'audio/eatgood.mp3');
+    this.load.audio("fail", 'audio/fail.mp3');
+    this.load.audio("pass", 'audio/pass.mp3');
+
+
+
+    // load start
+    this.load.on('progress', function (value) {
+        console.log(value);
+    });
+
+    this.load.on('fileprogress', function (file) {
+        if (file.src == "img/LODING1.gif") {
+            // const loadimg = game.physics.add.sprite(0, 0, 'load').setOrigin(0, 0);
+            console.log("okkook")
+        }
+        console.log(file.src);
+    });
+
+
+    this.load.on('complete', function () {
+        console.log('complete');
+    });
+
+
+    this.load.start();
+
+
+
 }
 
 function update() {
 
-    // 檢查食物超出邊界
-    foodOverBorders()
+    if (loadtime >= 2) {
 
-    // 背景移動
-    house1.setVelocityX(-800);
-    house2.setVelocityX(-800);
-    if (house1.body.x <= -3840) {
-        house1.body.x = 3840;
-    }
-    if (house2.body.x <= -3840) {
-        house2.body.x = 3840;
-    }
+        // 檢查食物超出邊界
 
-    sky1.setVelocityX(-600);
-    sky2.setVelocityX(-600);
-    if (sky1.body.x <= -3840) {
-        sky1.body.x = 3840;
-    }
-    if (sky2.body.x <= -3840) {
-        sky2.body.x = 3840;
-    }
+        foodOverBorders()
 
-    // 人物屬性更新
-    player.setVelocityX(0);
+        // 背景移動
+        house1.setVelocityX(-800);
+        house2.setVelocityX(-800);
+        if (house1.body.x <= -3840) {
+            house1.body.x = 3840;
+        }
+        if (house2.body.x <= -3840) {
+            house2.body.x = 3840;
+        }
 
-    // 鍵盤移動
-    if (cursors.up.isDown && player.body.touching.down && jump) {
-        player.setVelocityY(-1550);
-        jump = false;
-        setTimeout(() => jump = true, 800);
+        sky1.setVelocityX(-600);
+        sky2.setVelocityX(-600);
+        if (sky1.body.x <= -3840) {
+            sky1.body.x = 3840;
+        }
+        if (sky2.body.x <= -3840) {
+            sky2.body.x = 3840;
+        }
+
+        // 人物屬性更新
+        player.setVelocityX(0);
+
+        // 鍵盤移動
+        if (cursors.up.isDown && player.body.touching.down && jump) {
+            player.setVelocityY(-1550);
+            jump = false;
+            setTimeout(() => jump = true, 800);
+        }
     }
 
     // 測試用
